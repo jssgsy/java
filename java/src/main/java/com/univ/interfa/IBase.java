ebf373f3f3bf9e163cf91ca892941b9ee1128e6b
jssgsy <fcsnwu@163.com>
2016-01-20 10:46:41 +0800

    接口继承

---
+++ java/src/main/java/com/univ/interfa/IBase.java
@@ -0,0 +1,33 @@
+package com.univ.interfa;
+/** 
+ * @author univ 
+ * @date 2016年1月20日 上午9:58:01 
+ * @version v1.0
+ * @Description: 接口继承的演示
+ */
+public interface IBase {
+	String baseA();
+}
+
+/**
+ * 子接口可以不实现父接口的方法，相当于是继承了父接口的方法
+ */
+interface IDerive extends IBase{
+	int deriveA();
+}
+
+
+class A implements IDerive{
+
+	/**
+	 * 实现子接口的类除了实现子接口中的方法外还必须实现子接口的父接口中的方法
+	 */
+	public int deriveA() {
+		return 0;
+	}
+
+	public String baseA() {
+		return null;
+	}
+	
+}
--- java/src/main/java/com/univ/test/JTest.java
+++ java/src/main/java/com/univ/test/JTest.java
@@ -2,7 +2,6 @@ package com.univ.test;
 
 public class JTest {
 	
-	
 }
 
 
