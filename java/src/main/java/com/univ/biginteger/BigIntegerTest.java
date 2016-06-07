22096ff75131ca588678f04e715c2df371c731e1
jssgsy <fcsnwu@163.com>
2016-06-07 20:56:02 +0800

    修正大数对象的获取方法

--- java/src/main/java/com/univ/biginteger/BigIntegerTest.java
+++ java/src/main/java/com/univ/biginteger/BigIntegerTest.java
@@ -2,6 +2,7 @@ package com.univ.biginteger;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 /**
@@ -9,13 +10,18 @@ import java.math.BigInteger;
  * 16/6/7 10:32
  *
  * 1. 所有参加大数运算的数都必须是大数;
- * 2. 大数的产生只能通过BigInteger.valueOf(long val)获取,参数没有String类型,即不能从字符串转换;
+ * 2. 大数BigInteger对象产生的两种方法:
+ *      1. 通过BigInteger.valueOf(long val)获取,注意valueOf的参数没有String类型,即不能从字符串转换;
+ *      2. 利用构造函数,此时可以传入一个字符串;
+ * 3. 大数BigDecimal对象的获取方法是一样的;
  */
 public class BigIntegerTest {
 
     @Test
     public void powTest(){
-        BigInteger x = BigInteger.valueOf(2);
+//        BigInteger x = BigInteger.valueOf(2);//利用BigInteger.valueOf获取BigInteger对象
+        BigInteger x = new BigInteger("2");//利用构造函数传入String获取BigInteger对象
+
         int y = 80;
         BigInteger result = pow(x,y);
         System.out.println(result);
@@ -27,5 +33,19 @@ public class BigIntegerTest {
             result = result.multiply(x);
         }
         return result;
+    }
+
+    /**
+     * 两种构造BigDecimal对象的方法:
+     *  1. 利用BigDecimal.valueOf(double)或者其他类型;
+     *  2. 利用构造函数,传入String;
+     */
+    @Test
+    public void bigdecimal(){
+        BigDecimal b = BigDecimal.valueOf(2.44444444);
+        BigDecimal b1 = new BigDecimal("2.44444444");
+        System.out.println(b);
+        System.out.println(b1);
+
     }
 }
