8a0fb8ff4ff1022086d252d4740353327f2989ae
jssgsy <fcsnwu@163.com>
2016-06-18 15:00:17 +0800

    新增杂七杂八包

---
+++ java/src/main/java/com/univ/mess/MainTest.java
@@ -0,0 +1,18 @@
+package com.univ.mess;
+
+import org.junit.Test;
+
+/**
+ * created by Univ
+ * 16/6/18 14:28
+ */
+public class MainTest {
+
+
+    @Test
+    public void test(){
+        Sample s = new Sample();
+        System.out.println(s.getString());
+
+    }
+}
---
+++ java/src/main/java/com/univ/mess/Sample.java
@@ -0,0 +1,14 @@
+package com.univ.mess;
+
+/**
+ * created by Univ
+ * 16/6/18 14:29
+ *
+ * 非public类也可以另存为一个文件。可以被同包下的其他类使用。
+ */
+ class Sample {
+
+     String getString(){
+        return "sample";
+    }
+}
