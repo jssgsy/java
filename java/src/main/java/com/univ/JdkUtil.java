026a960eec1e0fd5c061cf02401e653a70117746
jssgsy <fcsnwu@163.com>
2016-10-12 00:53:51 +0800

    增强JdkUtil.java中Arrays方法的注释

--- java/src/main/java/com/univ/JdkUtil.java
+++ java/src/main/java/com/univ/JdkUtil.java
@@ -43,26 +43,29 @@ public class JdkUtil {
     }
 
     /**
-     * Arrays.toString
-     *  基本数据类型数组的排序。
-     *  Arrays中提供了基本数据类型数组排序:Arrays.sort(arr1,arr2)
+     * Arrays.toString()/Arrays.deepToString
      */
     @Test
     public void test2(){
         //int 类型数组排序
         int[] arrInt = {3, 5, 1, 39, 34, -3};
-        Arrays.sort(arrInt);
         System.out.println(Arrays.toString(arrInt));
 
         //double类型数组排序
         double[] arrDouble = {2.4, 1.2, 5, 36.2, 34.24, -3.5};
-        Arrays.sort(arrDouble);
         System.out.println(Arrays.toString(arrDouble));
 
         //String类型数组排序
         String[] arrStr = {"abd", "aefa", "feaf", "fjeifa", "iu", "aa", "iy", "etf"};
-        Arrays.sort(arrStr);
         System.out.println(Arrays.toString(arrStr));
+
+        //二维数组
+        int[][] arr2 = {
+                {1,3,5},
+                {3,23,7},
+                {23,36,14}
+        };
+        System.out.println(Arrays.deepToString(arr2));
     }
 
     /**
