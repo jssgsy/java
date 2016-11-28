8fd4144bf0c9aedb3fe5a39f2ac915cddb06e7e3
jssgsy <fcsnwu@163.com>
2016-11-28 19:41:18 +0800

    新增QuickFindTest.java

--- java/src/main/java/com/univ/algorithom/algorithms4/dynamic_connectivity/QuickFindTest.java
+++ java/src/main/java/com/univ/algorithom/algorithms4/dynamic_connectivity/QuickFindTest.java
@@ -1,9 +1,26 @@
 package com.univ.algorithom.algorithms4.dynamic_connectivity;
 
+import java.util.Scanner;
+
 /**
  * Univ
  * 16/11/28 18:50
  */
 public class QuickFindTest {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        QuickFind quickFind = new QuickFind(n);
+        while (in.hasNext()) {
+            int p = in.nextInt();
+            int q = in.nextInt();
+            if (quickFind.connected(p,q)) {
+                System.out.println(p + " 与 " + q + "已经连通");
+                continue;
+            }
+            quickFind.union(p,q);
+        }
+    }
 
 }
