fca4d7fb41c0ac7adba4b7b5d1e7baf166ebde96
univ <fcsnwu@163.com>
2018-12-06 09:49:07 +0800

    [stream]count方法与groupingBy方法

--- src/main/java/com/univ/jdk8/stream/StreamAPITest.java
+++ src/main/java/com/univ/jdk8/stream/StreamAPITest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 流式api，链式调用
@@ -23,5 +25,77 @@ public class StreamAPITest {
 
         // filter方法
         list.stream().filter(x -> x % 2 == 0).forEach(x -> System.out.print(x + " "));  // 2 4 6 8
+        System.out.println();
+
+        // count方法
+        long count = list.stream().filter(x -> x % 2 == 0).count();
+        System.out.println("count: " + count);
+
+    }
+
+    @Test
+    public void groupingBy() {
+        List<A> list = Arrays.asList(
+                new A("aaa", 10),
+                new A("bbb", 20),
+                new A("ccc", 20),
+                new A("ddd", 40),
+                new A("eee", 30),
+                new A("fff", 20),
+                new A("ggg", 40),
+                new A("ggg", 50),
+                new A("ggg", 10)
+        );
+
+        // 按照对象的age字段分组
+        Map<Integer, List<A>> collect = list.stream().collect(Collectors.groupingBy(t -> t.getAge()));
+        System.out.println(collect);
+        /*
+            {
+                50=[A{name='ggg', age=50}],
+                20=[A{name='bbb', age=20}, A{name='ccc', age=20}, A{name='fff', age=20}],
+                40=[A{name='ddd', age=40}, A{name='ggg', age=40}],
+                10=[A{name='aaa', age=10}, A{name='ggg', age=10}], 30=[A{name='eee', age=30}]
+            }
+         */
+
+    }
+}
+
+class A {
+    private String name;
+    
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public A(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public A() {
+    }
+
+    @Override
+    public String toString() {
+        return "A{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
