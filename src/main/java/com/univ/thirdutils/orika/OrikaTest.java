2f86538d2685086be6b483e559a1bf4ec31d0192
univ <fcsnwu@163.com>
2018-12-08 22:08:35 +0800

    [三方util][orika]自定义类型转换器

--- src/main/java/com/univ/thirdutils/orika/OrikaTest.java
+++ src/main/java/com/univ/thirdutils/orika/OrikaTest.java
@@ -1,6 +1,5 @@
 package com.univ.thirdutils.orika;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,7 +7,10 @@ import org.junit.Test;
 
 import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.MappingContext;
+import ma.glasnost.orika.converter.BidirectionalConverter;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
+import ma.glasnost.orika.metadata.Type;
 
 /**
  * @author univ
@@ -73,6 +75,39 @@ public class OrikaTest {
         mapperFacade.map(a,c);
         System.out.println(c);
 
+    }
+
+    /**
+     * orika的转换器：定义在两种类型中转换的规则。
+     * 现实需求：
+     * 1. 将数据库中的int，tinyint转换成java bean中的enum，
+     * 2. int与boolean互转;
+     * orika就是用来做这样事情的。
+     * 用法：
+     * 1. orika默认提供了很多转换器，如DateTimeFormatter， BidirectionalConverter(双向转换)等等;
+     * 2. 自定义转换器，需要继承orika提供的CustomConverter接口(单向转换)(还可以继承orika提供的现成的转换器）
+     * 3. 如果实现自定义转换器，则一般继承BidirectionalConverter实现双向转换即可
+     */
+    @Test
+    public void test3() {
+        DefaultMapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
+
+        // 全局注册
+        mapperFactory.getConverterFactory().registerConverter(new MyConvertor());
+
+        mapperFactory.classMap(D.class, E.class)
+                .field("num", "color")
+                .byDefault()
+                .register();
+        MapperFacade mapperFacade = mapperFactory.getMapperFacade();
+
+        // 演示int类型转换成某个具体的enum类型
+        E e = mapperFacade.map(new D(), E.class);
+        System.out.println(e);
+
+        // 演示某个具体的enum类型转换成int类型
+        Integer i = mapperFacade.map(Color.BLUE, Integer.class);
+        System.out.println(i);
     }
 }
 
@@ -224,3 +259,88 @@ class C {
     }
 }
 
+
+/**
+ * 继承自BidirectionalConverter便可实现双向转换，缓过泛型
+ */
+class MyConvertor extends BidirectionalConverter<Integer, Color>{
+
+    /**
+     * 将Integer转换成Color类型
+     * @param source
+     * @param destinationType
+     * @param mappingContext
+     * @return
+     */
+    @Override
+    public Color convertTo(Integer source, Type<Color> destinationType, MappingContext mappingContext) {
+        switch (source) {
+        case 1:
+            return Color.RED;
+        case 2:
+            return Color.GREEN;
+        default:
+            return Color.BLUE;
+        }
+    }
+
+    /**
+     * 将Color类型转换成int
+     * @param source
+     * @param destinationType
+     * @param mappingContext
+     * @return
+     */
+    @Override
+    public Integer convertFrom(Color source, Type<Integer> destinationType, MappingContext mappingContext) {
+        return source.ordinal();
+    }
+}
+
+class D {
+    private int num = 1;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return "D{" +
+                "num=" + num +
+                '}';
+    }
+}
+
+class E {
+    private Color color;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "E{" +
+                "color=" + color +
+                '}';
+    }
+}
+
+enum Color {
+
+    RED(1),GREEN(2),BLUE(3);
+    private int code;
+
+    Color(int code) {
+        this.code = code;
+    }
+}
