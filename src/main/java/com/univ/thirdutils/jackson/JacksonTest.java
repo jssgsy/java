f3e49011e9968862962bf281c298dbde894d573a
univ <fcsnwu@163.com>
2018-12-13 20:50:19 +0800

    [三方util][jackson]json字符串转成集合泛型

--- src/main/java/com/univ/thirdutils/jackson/JacksonTest.java
+++ src/main/java/com/univ/thirdutils/jackson/JacksonTest.java
@@ -3,8 +3,9 @@ package com.univ.thirdutils.jackson;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -12,6 +13,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
@@ -27,6 +29,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * 3. json字符串转对象：objectMapper.readValue();
  */
 public class JacksonTest {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     public void basic() throws IOException {
@@ -95,6 +99,69 @@ public class JacksonTest {
         // Set list1 = objectMapper.readValue(jsonStr, Set.class);
         System.out.println(list1);
         // [{name=aaa, age=20}, {name=bbb, age=30}, {name=ccc, age=40}]
+
+    }
+
+    /**
+     * json字符串转成List<T>
+     * 转成Set<T>也是类似
+     * @throws IOException
+     */
+    @Test
+    public void jsonToList() throws IOException {
+        
+        String jsonStr = "[{\"name\":\"aaa\",\"age\":20},{\"name\":\"bbb\",\"age\":30},{\"name\":\"ccc\",\"age\":40}]";
+        ObjectMapper objectMapper = new ObjectMapper();
+        // 转成list
+        List list = objectMapper.readValue(jsonStr, List.class);
+        System.out.println(list);
+        // [{name=aaa, age=20}, {name=bbb, age=30}, {name=ccc, age=40}]
+
+        // 转成List<User>
+        JavaType listType = objectMapper.getTypeFactory().constructCollectionType(List.class, User.class);
+        List<User> list1 = objectMapper.readValue(jsonStr, listType);
+        System.out.println(list1);
+        // [User{name='aaa', age=20}, User{name='bbb', age=30}, User{name='ccc', age=40}]
+        // 注意list1与list的差别
+    }
+
+    /**
+     * json字符串围成Map<K,V>
+     * @throws IOException
+     */
+    @Test
+    public void jsonToMap() throws IOException {
+        String mapStrJson = "{\"third\":{\"name\":\"ccc\",\"age\":40},\"first\":{\"name\":\"aaa\",\"age\":20},\"second\":{\"name\":\"bbb\",\"age\":30}}";
+        // 转成map
+        JavaType mapType = objectMapper.getTypeFactory().constructMapType(HashMap.class, String.class, User.class);
+        Map<String, User> map = objectMapper.readValue(mapStrJson, mapType);
+        System.out.println(map);
+        // {third=User{name='ccc', age=40}, first=User{name='aaa', age=20}, second=User{name='bbb', age=30}}
+    }
+
+    /**
+     * json字符串转成集合泛型
+     * @throws IOException
+     */
+    @Test
+    public void jsonToMighty() throws IOException {
+        /*Author author = new Author();
+        author.setAge(20);
+        author.setName("abc");
+
+        Book<Author> book = new Book<>();
+        book.setValue(author);
+        book.setName("book");
+
+        String str = objectMapper.writeValueAsString(book);
+        System.out.println(str);*/
+        // {"name":"book","value":{"name":"abc","age":20}}
+
+        String jsonStr = "{\"name\":\"book\",\"value\":{\"name\":\"abc\",\"age\":20}}";
+        JavaType javaType = objectMapper.getTypeFactory().constructParametricType(Book.class, Author.class);
+        Book<Author> book1 = objectMapper.readValue(jsonStr, javaType);
+        System.out.println(book1);
+        // Book{name='book', value=Author{name='abc', age=20}}
 
     }
 }
@@ -172,5 +239,64 @@ class User1 {
 
     public void setBirthday(Date birthday) {
         this.birthday = birthday;
+    }
+}
+
+class Book<T> {
+    private String name;
+    private T value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
+
+class Author {
+    private String name;
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
