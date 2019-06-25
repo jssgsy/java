a838a7e07cf20b1c7e3c15a6180b985af09ed865
univ <fcsnwu@163.com>
2019-06-25 16:45:19 +0800

    [jdk][thirdutils]mockito补充

---
+++ src/main/java/com/univ/thirdutils/mockito/BasicMockitoTest.java
@@ -0,0 +1,211 @@
+package com.univ.thirdutils.mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * @author univ
+ * @date 2019/6/25 3:23 PM
+ * @description 最基本的mockito的使用方法
+ *
+ *  一般使用 Mockito 需要执行下面三步
+ *  1. 模拟并替换测试代码中外部依赖：使用mock方法或者相应的注解
+ *  2. 执行测试代码
+ *  3. 验证测试代码是否被正确的执行
+ *
+ * 核心：单测，针对的是一个个独立的方法及其中的业务逻辑，其中所有依赖的方法都应视为外部依赖，应该mock掉。
+ *  即使是调用的内部的其它方法，此方法也应该给mock掉，因为其自有单测来保证
+ */
+public class BasicMockitoTest {
+
+    /**
+     * 1. 被mock对象可以理解成一个代理，其方法的返回值可以指定
+     * 2. 只要使用了一个参数匹配器(argument matcher)，则所有参数都需要使用参数匹配器
+     */
+    @Test
+    public void test1() {
+        // mock一个OrdinaryObj对象（也可以mock具体的类），此时其方法不会实际被调用
+        Ordinary ordinary = mock(Ordinary.class);
+        boolean b = ordinary.isOrdinary();
+        // 返回相应的零值(By default, for all methods that return a value,
+        // a mock will return either null, a primitive/primitive wrapper value,
+        // or an empty collection, as appropriate. For example 0 for an int/Integer and false for a boolean/Boolean)
+        System.out.println(b);
+
+        // 这个是ok的，anyString(), anyInt()都是参数匹配器
+        verify(ordinary).getList(anyString(), anyInt());
+
+        // 会抛异常，因为12不是参数匹配器
+        // verify(demoDao).getByNameAndAge(anyString(), 12);
+    }
+
+    /**
+     * 根据不同的输入返回不同的值
+     */
+    @Test
+    public void test2() {
+        Ordinary ordinary = mock(Ordinary.class);
+        when(ordinary.getType(1)).thenReturn("aaa");
+        when(ordinary.getType(2)).thenReturn("bbb");
+        assertEquals("竟然失败了", "aaa", ordinary.getType(1));
+        assertEquals("竟然失败了", "bbb", ordinary.getType(2));
+
+        /**
+         * 返回值不依赖于入参
+         * anyString()
+         * anyint()
+         * ...
+         * any()
+         */
+        when(ordinary.getType(anyInt())).thenReturn("hello");
+        assertEquals("竟然失败了", "hello", ordinary.getType(3));
+    }
+
+    /**
+     * 多次调用，返回不同的结果，可应用在测试根据返回值的不同走不同分支时，是一种简便写法
+     */
+    @Test
+    public void test3() {
+        Ordinary ordinary = mock(Ordinary.class);
+        // 第一次调用ordinary.getType(1)时返回"aaa"，第二次调用时返回"bbb"
+        when(ordinary.getType(1)).thenReturn("aaa").thenReturn("bbb");
+
+        assertEquals("aaaa", ordinary.getType(1));
+        assertEquals("bbb", ordinary.getType(1));
+
+        // 等价的写法
+        when(ordinary.getType(1)).thenReturn("aaa", "bbb");
+    }
+
+    /**
+     * 调用某个方法时抛异常，用在某个方法不允许被调用
+     * doThrow(ExceptionX).when(x).methodCall
+     */
+    @Test
+    public void test4() {
+        Ordinary ordinary = mock(Ordinary.class);
+        // 注意语法形式，对象和方法是分开的，与when.doReturn不同
+        doThrow(new RuntimeException("此方法不能被调用")).when(ordinary).isOrdinary();
+
+        // 下行代码会抛异常
+        // ordinary.isOrdinary();
+    }
+
+    /**
+     * 验证方法是否被调用
+     */
+    @Test
+    public void test5() {
+        Ordinary ordinary = mock(Ordinary.class);
+
+        // 调用方法
+        ordinary.getById(1);
+        ordinary.isOrdinary();
+        ordinary.isOrdinary();
+        ordinary.isOrdinary();
+
+        // 验证当传1时getById方法是否被调用
+        verify(ordinary).getById(1);
+
+        // 验证isOrdinary方法被调用了三次，否则抛异常
+        verify(ordinary, times(3)).isOrdinary();
+
+        // 验证getName()方法一次都没被调用过
+        // ordinary.getName();
+        verify(ordinary, never()).setId();
+
+        /*
+        类似的方法有：
+            atLeastOnce();// 至少被调用一次
+            atLeast(3); // 至少被调用3次
+            atMost(3);  // 最多被调用3次
+         */
+    }
+
+    /**
+     * spy 重要
+     *  mock方法是模拟出一个“替代”对象，而spy是模拟出一个“真实”的对象，即其方法会真正被调用
+     *  spy方法可以包装一个真实的Java对象, 并返回一个包装后的新对象. 若没有特别配置的话, 对这个新对象的所有方法调用, 都会委派给实际的 Java 对象
+     */
+    @Test
+    public void test6() {
+        // 模拟出一个真正的OrdinaryObject对象，一般用在具体的类而不是接口上，因为就是想执行其真实代码
+        OrdinaryObject ordinary = spy(OrdinaryObject.class);
+        // 此时会调用真实的fn方法，可调试进入
+        ordinary.fn();
+
+        // 被spy的对象也可以mock其方法，但不要对spy对象调用when.thenReturn，而是使用doReturn.when模式
+        List list = new LinkedList();
+        List spy = spy(list);
+        //Impossible: real method is called so spy.get(0) throws IndexOutOfBoundsException (the list is yet empty)
+        // 意思是说，这种形式下，spy.get(0)会实际被调用一次(在thenReturn前)，此时就报错了，所以需要doReturn.when的形式
+        when(spy.get(0)).thenReturn("foo");
+        //You have to use doReturn() for stubbing
+        Mockito.doReturn("foo").when(spy).get(0);
+    }
+
+    /**
+     * 重点：spy的真实应用，即部分方法mock，其它部分实际执行
+     * 假设要测试service中的方法fn，fn又调用了此service中的其它方法fn1，那么在测试fn时，fn1其实也是一个依赖，需要mock掉！
+     * (fn1有针对自己的单测，但在测试fn时，其就是一个外部依赖)
+     */
+    @Test
+    public void test7() {
+        OrdinaryObject ordinary = spy(OrdinaryObject.class);
+        // 注意，记得使用doReturn.when(obj).method的形式，不要使用when.return的方式
+        doReturn("重点关注，关注要单测的方法的逻辑，其它所有调用都mock掉，不论是外部还是内部").when(ordinary).fn1();
+        ordinary.fn1();
+    }
+
+}
+
+/**
+ * 基本对象
+ */
+interface Ordinary {
+
+    boolean isOrdinary();
+
+    Ordinary getById(int id);
+
+    String getType(Integer code);
+
+    String getName(Ordinary demo);
+
+    List getList(String name, int age);
+
+    void setId();
+}
+
+class OrdinaryObject {
+
+    public String fn() {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        return "aaa";
+    }
+
+    public void fn1() {
+        String a = "aaa";
+        // 此方法
+        String fn = fn();
+        String b = "bbb" + fn;
+    }
+}
+
--- src/main/java/com/univ/thirdutils/mockito/MockitoTest.java
+++ src/main/java/com/univ/thirdutils/mockito/MockitoTest.java
@@ -1,23 +1,8 @@
 package com.univ.thirdutils.mockito;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.anyInt;
-import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.util.LinkedList;
-import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -25,21 +10,14 @@ import org.mockito.MockitoAnnotations;
 /**
  * @author univ
  * @datetime 2018/12/10 9:38 PM
- * @description
- *
- * 一般使用 Mockito 需要执行下面三步
- *  1. 模拟并替换测试代码中外部依赖：使用mock方法或者相应的注解
- *  2. 执行测试代码
- *  3. 验证测试代码是否被正确的执行
- *
- * 注意：
- *  1. 被mock对象的方法不会被实际执行，所以不能进到mock对象方法内部进行调试(也不需要)；
+ * @description mockito使用注解
  */
 /*
 mockito的常用方法：
 1. mock：mock出一个对象；
 2. when(mock.someMethod()).thenReturn(X);设定当执行mock.someMethod()方法时返回X
  */
+// @RunWith(MockitoJUnitRunner.class)
 public class MockitoTest {
 
     @Before
@@ -47,205 +25,26 @@ public class MockitoTest {
         // 有了此句代码，则会自动mock出被@Mock, @Spy, @Captor, @InjectMocks注解的对象
         // 此时使用@Mock, @Spy, @Captor, @InjectMocks时不用再显示new了
         MockitoAnnotations.initMocks(this);
+
+        // 在测试类上使用@RunWith(MockitoJUnitRunner.class)也能达到一样的效果
     }
 
     /**
-     * 使用mock方法模拟的对象的方法不会被真正执行，所以测试时也进不了debug模式，
-     * 通过@InjectMocks模拟的对象就有了（spy）的功能，即会被真正执行，适合用来模拟service层对象，毕竟单测测的就是service层对象的业务方法，如果不能走进去则单测便也没了意义。
-     * 注意，这里要new Demo()一下
+     * 通过@InjectMocks模拟的对象就有了（spy）的功能，即会被真正执行，适合用来模拟单测的对象
      */
     @InjectMocks
-    private Demo demo = new Demo();
+    private DemoService demoService;
 
+    // 此时对象demoDao会被装配到对象demoService中，这个就是@InjectMocks的作用之一
     @Mock
     private DemoDao demoDao;
 
-    @InjectMocks
-    private A a;
-
-    // 此时对象b会被装配到对象a中
-    @Mock
-    private B b;
-
-    /**
-     * 从官网基本学习的基本知识
-     */
-    @Test
-    public void basic() {
-        // 只要使用了一个参数匹配器(argument matcher)，则所有参数都需要使用参数匹配器
-        DemoDao demoDao = mock(DemoDao.class);
-        demoDao.getByNameAndAge("aaa", 20);
-
-        // 这个是ok的，anyString(), anyInt()都是参数匹配器
-        verify(demoDao).getByNameAndAge(anyString(), anyInt());
-
-        // 会抛异常，因为12不是参数匹配器
-        // verify(demoDao).getByNameAndAge(anyString(), 12);
-        // ---------------------------------------------------------------------------------
-
-
-        // 调用mock对象的任何非stubbed的方法(when.thenReturn),都将返回相应的零值(By default, for all methods that return a value, a mock will return either null, a primitive/primitive wrapper value, or an empty collection, as appropriate. For example 0 for an int/Integer and false for a boolean/Boolean)
-        //following prints "null" because demoDao.getById was not stubbed
-        System.out.println(demoDao.getById(anyInt()));
-        // ---------------------------------------------------------------------------------
-
-
-        // 同样的调用返回不同的结果
-        Demo d1 = new Demo();
-        Demo d2 = new Demo();
-        when(demoDao.getById(1)).thenReturn(d1).thenReturn(d2);
-        // 第一次调用返回d1,第二次调用返回d2
-        assertEquals(d1, demoDao.getById(1));
-        assertEquals(d2, demoDao.getById(1));
-
-        // 或者可以使用如下的写法
-        when(demoDao.getById(2)).thenReturn(d2, d1);
-        // 第一次调用返回d1,第二次调用返回d2
-        assertEquals(d2, demoDao.getById(2));
-        assertEquals(d1, demoDao.getById(2));
-        // ---------------------------------------------------------------------------------
-
-
-        // stub没有返回值的方法:doThrow()、doNothing()
-        // 上述的when.thenReturn都是用来stub有返回值的方法
-        doThrow(new RuntimeException("此方法不能被调用")).when(demoDao).setId();
-        demoDao.setId();
-    }
-
     @Test
     public void test1() {
-
-        // 设定，当代码中调用demoDao.getById方法，且并传入任何int类型的参数时返回demo对象
-        Mockito.when(b.getName(anyString())).thenReturn("aaa");
-
-        /**
-         * 1. 核心：内部调用了b.getName("hello")，假如此方法是需要向数据库获取的，在测试环境下，往往不能或者很难从数据库、缓存、dubbo等获取数据源，单元测试的核心就在于mock掉这些依赖于环境的对象，并设定将这些对象的方法被调用时返回指定的结果，这样就可以独立于这些环境对象来测试自己的业务流程！单元测试测试的是自己的业务流程
-         * 2. 上面已经设定了当调用b.getName时会返回aaa，因此当业务方法business中依赖了b.getName时就能使流程往下走
-         */
-        a.business();
+        // mock掉外部依赖
+        Mockito.when(demoDao.getName(Mockito.anyInt())).thenReturn("aaa");
+        demoService.fn();
     }
-    
-    @Test
-    public void test2() {
-        Demo demo = mock(Demo.class);
-
-        /*
-        根据不同的输入返回不同的值
-         */
-        when(demo.sayHello("aaa")).thenReturn("aaa");
-        when(demo.sayHello("bbb")).thenReturn("bbb");
-        assertEquals("竟然失败了", "aaa", demo.sayHello("aaa"));
-        assertEquals("竟然失败了", "bbb", demo.sayHello("bbb"));
-
-        /**
-         * 返回值不依赖于入参
-         * anyString()
-         * anyint()
-         * ...
-         * any()
-         */
-        when(demo.sayHello(anyString())).thenReturn("hello");
-        assertEquals("竟然失败了", "hello", demo.sayHello("xyz"));
-    }
-
-    /**
-     * 验证函数是否被调用
-     * Mockito 会跟踪 mock 对象里面所有的方法和变量。所以我们可以用来验证函数在传入特定参数的时候是否被调用。这种方式的测试称行为测试，行为测试并不会检查函数的返回值，而是检查在传入正确参数时候函数是否被调用。
-     *
-     */
-    @Test
-    public void test3() {
-        Demo demo = mock(Demo.class);
-
-        // 调用方法
-        demo.sayHello("aaa");
-        demo.getAge();
-        demo.getAge();
-        demo.getAge();
-
-        // 验证当传“aaa”时sayHello方法是否被调用
-        verify(demo).sayHello(Matchers.eq("aaa"));
-
-        // 验证getAge()方法被调用了三次，否则抛异常
-        verify(demo, times(3)).getAge();
-
-        // 验证getName()方法一次都没被调用过
-        // demo.getName();
-        verify(demo, never()).getName();
-
-
-        /*
-        类似的方法有：
-            atLeastOnce();// 至少被调用一次
-            atLeast(3); // 至少被调用3次
-            atMost(3);  // 最多被调用3次
-         */
-    }
-
-    /**
-     * spy
-     *  mock方法是模拟出一个“替代”对象，而spy是模拟出一个“真实”的对象，即其方法会真正被调用
-     *  spy方法可以包装一个真实的Java对象, 并返回一个包装后的新对象. 若没有特别配置的话, 对这个新对象的所有方法调用, 都会委派给实际的 Java 对象
-     */
-    @Test
-    public void test4() {
-
-        // 模拟出一个真正的DemoService对象
-        DemoService demoService = spy(DemoService.class);
-
-        // 需要这句代码是因为DemoService内部有DemoDao对象，getById方法内部会调用到
-        demoService.setDemoDao(demoDao);
-
-
-        when(demoDao.getById(anyInt())).thenReturn(new Demo());
-
-        // 此时会调用真正的sayHello方法，因此可以调试进入
-        demoService.sayHello(23);
-
-        // 被spy的对象也可以stub其方法，但这是非常不好的实践，即不要对spy对象调用when.thenReturn，实在要这样做的时候使用doReturn.when模式
-        List list = new LinkedList();
-        List spy = spy(list);
-        //Impossible: real method is called so spy.get(0) throws IndexOutOfBoundsException (the list is yet empty)
-        when(spy.get(0)).thenReturn("foo");
-        //You have to use doReturn() for stubbing
-        Mockito.doReturn("foo").when(spy).get(0);
-    }
-
-    /**
-     * 调试
-     */
-    @Test
-    public void test5() {
-        // 这里可以打断点进入调试,因为demo是被@InjectMocks注入的，有stub的功能
-        demo.sayHello("world");
-    }
-
-    /**
-     * doThrow(ExceptionX).when(x).methodCall
-     *  含义是: 当调用了 x.methodCall 方法后, 抛出异常 ExceptionX
-     */
-    @Test
-    public void test6() {
-        Demo demo = mock(Demo.class);
-
-        // 设定：当调用demo对象的sayHello方法时抛出异常
-        doThrow(new RuntimeException("不能调用此方法")).when(demo).sayHello(anyString());
-
-        // 调用sayHello方法，此时会抛异常
-        // demo.sayHello("aaa");
-    }
-
-    /**
-     * 演示 @InjectMocks与@Mock的重要作用
-     */
-    @Test
-    public void test7() {
-        // 在这里打断点
-
-        // when(b.getName(anyString())).thenReturn("aaaa");
-        a.business();
-    }
-
 
 }
 
@@ -253,23 +52,13 @@ public class MockitoTest {
  * 模拟service
  */
 class DemoService {
-
     private DemoDao demoDao;
 
-    public String sayHello(int id) {
-        // 业务逻辑处理
-
-        // 从数据库、索引，dubbo等获取数据源
-        Demo demo = demoDao.getById(id);
-        System.out.println(demo);
-
-        // 业务逻辑处理
-        
-        return "hello";
-    }
-
-    public void setDemoDao(DemoDao demoDao) {
-        this.demoDao = demoDao;
+    public void fn() {
+        int i = 1;
+        int j = 1;
+        String name = demoDao.getName(100);
+        System.out.println(name);
     }
 }
 
@@ -277,76 +66,5 @@ class DemoService {
  * 模拟dao
  */
 interface DemoDao {
-
-    Demo getById(int id);
-
-    List<Demo> getByDemo(Demo demo);
-
-    List<Demo> getByNameAndAge(String name, int age);
-
-    public void setId();
-}
-
-/**
- * 模拟实体
- */
-class Demo{
-    private String name;
-    private int age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String sayHello(String name) {
-        return "hello, " + name;
-    }
-
-    @Override
-    public String toString() {
-        return "Demo{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-}
-
-/**
- * 业务类
- */
-class A {
-    private B b;
-    // 这里是需要测试的业务逻辑
-    public void business() {
-        // 业务逻辑处理
-
-        // 依赖的数据源对象
-        String result = b.getName("hello");
-        System.out.println(result);
-
-
-        // 业务逻辑处理
-    }
-}
-
-/**
- * 业务类A依赖的“数据源”对象
- */
-class B {
-    public String getName(String name) {
-        return name;
-    }
-
+    String getName(int id);
 }
