c6c784297142f723b100b18924fd1e7e75876007
univ <fcsnwu@163.com>
2019-07-24 18:15:37 +0800

    [java]Executor

---
+++ src/main/java/com/univ/multithread/executor/ExecutorServiceTest.java
@@ -0,0 +1,42 @@
+package com.univ.multithread.executor;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.junit.Test;
+
+/**
+ * @author univ
+ * @date 2019/7/24 4:47 PM
+ * @description
+ * 核心对象：
+ *  Executor：能执行提交任务的对象(接口)；抽象级别高；
+ *  ExecutorService：接口，是Executor的子接口，抽象级别高，先理解成线程池(ThreadPoolExecutor是其实现类之一)；
+ *  Executors：工具类
+ */
+public class ExecutorServiceTest {
+
+    @Test
+    public void test() throws ExecutionException, InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+        Future<Integer> result = threadPool.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                System.out.println("当前线程名称:" + Thread.currentThread().getName());
+                Thread.sleep(3000l);
+                System.out.println("休眠结束");
+                return 10;
+            }
+        });
+        System.out.println("before get----");
+        // 会阻塞直到执行完成
+        System.out.println(result.get());
+        System.out.println("after get----");
+    }
+
+
+
+}
---
+++ src/main/java/com/univ/multithread/future/README.md
@@ -0,0 +1,1 @@
+﻿﻿![Future释义](https://github.com/jssgsy/java/raw/master/src/main/java/com/univ/multithread/future/future.png)
