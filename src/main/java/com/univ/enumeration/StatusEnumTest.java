35013a1902383cda3d41873e5e5b8bf9d279d7ef
univ <fcsnwu@163.com>
2019-11-21 20:17:07 +0800

    [enum]fix space

--- src/main/java/com/univ/enumeration/StatusEnumTest.java
+++ src/main/java/com/univ/enumeration/StatusEnumTest.java
@@ -1,5 +1,7 @@
 package com.univ.enumeration;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class StatusEnumTest {
@@ -8,11 +10,31 @@ public class StatusEnumTest {
     public void test() {
         System.out.println(StatusEnum.STATUS_OFF.getKey());
         System.out.println(StatusEnum.STATUS_OFF.getValue());
+
+        StatusEnum anEnum = StatusEnum.getByKey(10);
+
+        // 对enum使用switch前注意要先判断非空，否则NPE
+        if (null != anEnum) {
+            switch (anEnum) {
+            case STATUS_OFF:
+                System.out.println("off");
+                break;
+            case STATUS_ON:
+                System.out.println("on");
+                break;
+            }
+        }
+
     }
 }
 
 enum StatusEnum {
-    STATUS_ON(1, "开启"),STATUS_OFF(0, "关闭");
+    STATUS_ON(1, "开启"),
+    STATUS_OFF(0, "关闭");
+
+    public static StatusEnum getByKey(Integer key) {
+        return Arrays.stream(values()).filter(t -> t.getKey().equals(key)).findFirst().orElse(null);
+    }
 
     private Integer key;
     private String value;
@@ -29,6 +51,8 @@ enum StatusEnum {
     public String getValue() {
         return value;
     }
+
+
 }
 
 
