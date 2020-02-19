0a26269cdaf1e793e5f809f027f5c729a06e8068
univ <fcsnwu@163.com>
2020-02-19 21:14:21 +0800

    [power mockito]mock父类方法

--- src/main/java/com/univ/thirdutils/powermockito/PowerMockitoTest.java
+++ src/main/java/com/univ/thirdutils/powermockito/PowerMockitoTest.java
@@ -35,7 +35,65 @@ public class PowerMockitoTest {
         PowerMockito.when(StaticMethod.staticMethod2()).thenReturn("mock的静态方法值2");
         staticMethod.fn();
     }
-    
+
+    /**
+     * 子类方法与父类方法不同名,mockito也可用相同的方法完成
+     */
+    @Test
+    public void testExtend1() {
+        // 注意要定义在使用之前
+        class Parent {
+            public String fn(){
+                System.out.println("进入父类fn---");
+                System.out.println("离开父类fn---");
+                return "父类fn()真实值";
+            }
+        }
+        class Child extends Parent {
+            public String fn1(){
+                System.out.println("进入子类fn1---");
+                String fn = fn(); // 注意此时不能使用super.fn()方法，否则PowerMockito.doReturn("mock值").when(spy).fn();失效
+                System.out.println("调用父类fn()的返回值为：" + fn);
+                System.out.println("离开子类fn1---");
+                return "父类fn()真实值";
+            }
+        }
+
+        Child spy = PowerMockito.spy(new Child());
+        // 只要调用父类方法时没加super关键字就行，其实和Mockito是一样的用法
+        PowerMockito.doReturn("mock值").when(spy).fn();
+        spy.fn1();
+    }
+
+    /**
+     * 子类方法与父类方法同名, 此时mockito无能为力，需要power mockito出场
+     */
+    @Test
+    public void testExtend2(){
+        Child spy = PowerMockito.spy(new Child());
+
+        // 注意要定义在使用之前，注意，实践表明，此时不能将类定义在方法中，否则失效
+        PowerMockito.suppress(PowerMockito.methodsDeclaredIn(Parent.class));
+        spy.ff();
+    }
+
+    class Parent {
+        public String ff(){
+            System.out.println("进入父类ff---");
+            System.out.println("离开父类ff---");
+            return "父类ff()真实值";
+        }
+    }
+    class Child extends Parent {
+        @Override
+        public String ff() {
+            System.out.println("进入子ff---");
+            String ff = super.ff(); // 注意这里带了super关键字
+            System.out.println("调用父类fn()的返回值为：" + ff);
+            System.out.println("离开子ff---");
+            return "父类ff()真实值";
+        }
+    }
 }
 
 class PrivateMethod {
@@ -88,3 +146,4 @@ class StaticMethod {
         return "staticMethod2真实值";
     }
 }
+
