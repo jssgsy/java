fb6c2d5faf5a919dd28a62294b05d6ce6848fce9
liuminglu <liuminglu@youzan.com>
2021-04-05 20:03:52 +0800

    [jdk-内省]PropertyDescriptorTest

---
+++ src/main/java/com/univ/jdkutils/introspector/PropertyDescriptorTest.java
@@ -0,0 +1,37 @@
+package com.univ.jdkutils.introspector;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.junit.Test;
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class PropertyDescriptorTest {
+
+    @Test
+    public void test() throws IntrospectionException, InvocationTargetException, IllegalAccessException {
+        Person person = new Person();
+        PropertyDescriptor propertyDescriptor = new PropertyDescriptor("name", Person.class);
+        // 获取getter方法
+        Method readMethod = propertyDescriptor.getReadMethod();
+        // 用getter方法获取值
+        System.out.println("属性name的旧值为：" + readMethod.invoke(person));
+        // 获取setter方法
+        Method writeMethod = propertyDescriptor.getWriteMethod();
+        // 用setter方法设值
+        writeMethod.invoke(person, "abc");
+
+        System.out.println("属性name的旧值为：" + readMethod.invoke(person));
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    class Person {
+        private String name;
+    }
+}
