d44c60067eeade33723727a5147e1656ddda05d1
univ <fcsnwu@163.com>
2021-04-07 10:52:52 +0800

    [算法]重试-重试策略

--- src/main/java/com/univ/algorithom/retry/SimpleRetryTest.java
+++ src/main/java/com/univ/algorithom/retry/SimpleRetryTest.java
@@ -29,7 +29,7 @@ public class SimpleRetryTest {
      * @return
      */
     public boolean fnWithBoolean() {
-        System.out.println("fnWithBoolean");
+        System.out.println("fnWithBoolean被调用了-重试时机-返回了false");
         // return false;
 
         // 放开这句，也会重试
@@ -38,10 +38,10 @@ public class SimpleRetryTest {
 
     /**
      * 待重试方法-重试时机-抛出异常
-     * @return
+     * @return 此返回值没任何意义，仅仅是为了call方法能统一三种场景
      */
-    public void fnWithException() {
-        System.out.println("fnWithException");
+    public Object fnWithException() {
+        System.out.println("fnWithException被调用了-重试时机：抛出异常");
         throw new RuntimeException("fnWithException抛异常了");
     }
 
@@ -50,7 +50,7 @@ public class SimpleRetryTest {
      * @return
      */
     public Integer fnWithReturnResult() {
-        System.out.println("fnWithReturnResult被调用了");
+        System.out.println("fnWithReturnResult被调用了-重试时机：返回值不符合预期");
         return 10;
     }
 
@@ -58,10 +58,9 @@ public class SimpleRetryTest {
      * 场景1：接口返回值为false时重试
      *
      * 兼容了异常时也重试
-     * @throws Exception
      */
     @Test
-    public void testWithBoolean() throws Exception {
+    public void testWithBoolean() {
         SimpleRetryHelper retryHelper = new SimpleRetryHelper();
         retryHelper.callWithBoolean(this::fnWithBoolean);
     }
@@ -79,10 +78,9 @@ public class SimpleRetryTest {
      * 场景3：接口返回值不符合预期时重试。兼容了场景1
      *
      * 兼容了异常时也重试
-     * @throws Exception
      */
     @Test
-    public void testWithReturnResult() throws Exception {
+    public void testWithReturnResult() {
         SimpleRetryHelper retryHelper = new SimpleRetryHelper(3, new SleepRetryStrategy(2000L));
         retryHelper.callWithReturnResult(this::fnWithReturnResult, t -> {
             Integer t1 = (Integer) t;
@@ -90,8 +88,43 @@ public class SimpleRetryTest {
         });
     }
 
-    interface RetryStrategy {
+    /**
+     * 上述场景一、二、三的结合体
+     */
+    @Test
+    public void testCall() {
+        SimpleRetryHelper retryHelper = new SimpleRetryHelper(3);
+        // 模拟场景1
+        retryHelper.call(this::fnWithBoolean, t -> {
+            // 发生异常时t为null
+            if (null == t) {
+                return false;
+            }
+            return (Boolean) t;
+        });
+        System.out.println("-----换行-----");
 
+        // 模拟场景2
+        retryHelper = new SimpleRetryHelper(3);
+        retryHelper.call(this::fnWithException, null);
+        System.out.println("-----换行-----");
+
+        // 模拟场景3
+        retryHelper = new SimpleRetryHelper(3);
+        retryHelper.call(this::fnWithReturnResult, t -> {
+            // 发生异常时结果t为null
+            if (null == t) {
+                return false;
+            }
+            Integer t1 = (Integer) t;
+            return t1 > 10;
+        });
+    }
+
+    /**
+     * 重试策略：重试前需要做的事情，如什么也不做，或者休眠一会再重试等
+     */
+    interface RetryStrategy {
         void beforeRetry();
     }
 
@@ -127,6 +160,8 @@ public class SimpleRetryTest {
 
     /**
      * 重试类
+     *
+     * 非线程安全：即不要将此类作为共享资源使用，亦即不要当作单例对象使用
      */
     class SimpleRetryHelper {
         /**
@@ -172,6 +207,8 @@ public class SimpleRetryTest {
                 result = callable.call();
             } catch (Exception exception) {
                 retryWithBoolean(callable);
+
+                return;
             }
             if (null == result || !result) {
                 retryWithBoolean(callable);
@@ -181,7 +218,7 @@ public class SimpleRetryTest {
         /**
          * 当接口的调用返回结果不符合预期时重试
          * @param callable  可能会重试的目标逻辑
-         * @param predicate 是否要重试的判断逻辑
+         * @param predicate 是否要重试的判断逻辑，也可作为一个类字段，这样的好处是不用每次使用时都传入一遍
          * @throws Exception
          */
         public void callWithReturnResult(Callable callable, Predicate predicate) {
@@ -191,10 +228,57 @@ public class SimpleRetryTest {
                 result = callable.call();
             } catch (Exception exception) {
                 retryWithReturnResult(callable, predicate);
+
+                // 这句不要忘了，否则下面的语句可能会执行
+                return;
             }
 
             if (!predicate.test(result)) {
                 retryWithReturnResult(callable, predicate);
+            }
+        }
+
+        /**
+         * 上述三种场景的结合体，这样对外只需要暴露一个方法而已
+         *
+         * 方法作用：执行callable，执行过程中发生异常，或者执行结果不符合预期时重试。
+         *
+         * 注：因为这里的predicate只在需要根据返回值是否满足条件而重试，因此可以将其作为类的成员变量，这样对外暴露可以少一个参数
+         * @param callable Callable有返回值，因此可兼容抛出异常时的无返回值情况
+         * @param predicate
+         */
+        public void call(Callable callable, Predicate predicate) {
+            Object result;
+            try {
+                result = callable.call();
+            } catch (Exception exception) {
+                // 兼容异常情况，此时和返回值已经没关系了
+                retryCall(callable, predicate);
+
+                // 这句不要忘了，否则下面的语句可能会执行
+                return;
+            }
+            if (null != predicate && !predicate.test(result)) {
+                retryCall(callable, predicate);
+            }
+        }
+
+        private void retryCall(Callable callable, Predicate predicate) {
+            if (currentRetryNum >= maxRetryNum) {
+                return;
+            }
+            currentRetryNum++;
+            System.out.println("当前重试第【" + currentRetryNum + "】次，且上次调用不符合预期(可能是返回值有误，可能抛出了异常)，需要再次重试。。。");
+            Object result;
+            try {
+                result = callable.call();
+            } catch (Exception exception) {
+                retryCall(callable, predicate);
+
+                return;
+            }
+            if (null != predicate && !predicate.test(result)) {
+                retryCall(callable, predicate);
             }
         }
 
@@ -211,6 +295,8 @@ public class SimpleRetryTest {
                 result = callable.call();
             } catch (Exception exception) {
                 retryWithReturnResult(callable, predicate);
+
+                return;
             }
 
             if (!predicate.test(result)) {
