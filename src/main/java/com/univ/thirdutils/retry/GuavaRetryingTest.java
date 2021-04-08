666ab17f6be1a471d97a66e9d89af8f7fe633196
univ <fcsnwu@163.com>
2021-04-08 20:09:06 +0800

    [重试]spa

--- src/main/java/com/univ/thirdutils/retry/GuavaRetryingTest.java
+++ src/main/java/com/univ/thirdutils/retry/GuavaRetryingTest.java
@@ -21,26 +21,37 @@ import com.google.common.base.Predicates;
  * 2. 使用retryer.call(callable);
  */
 public class GuavaRetryingTest {
-    private Retryer<Boolean> retryer = RetryerBuilder.<Boolean>newBuilder()
-            .retryIfResult(Predicates.isNull()) // Callable返回null就重试
-            .retryIfResult(aBoolean -> Objects.equals(aBoolean, Boolean.FALSE)) // Callable返回false就重试
-            .retryIfRuntimeException()  // Callable抛出运行时异常就重试
-            .withWaitStrategy(WaitStrategies.fixedWait(1, TimeUnit.SECONDS))
-            .withStopStrategy(StopStrategies.stopAfterAttempt(3))   // 如果Stop的话就直接抛出RetryException异常了
-            .build();
 
     @Test
-    public void basic() {
+    public void testBoolean() throws ExecutionException, RetryException {
+        Retryer<Boolean> retryer = RetryerBuilder.<Boolean>newBuilder()
+                .retryIfResult(Predicates.isNull()) // Callable返回null就重试
+                .retryIfResult(result -> Objects.equals(result, Boolean.FALSE)) // Callable返回false就重试
+                .retryIfRuntimeException()  // Callable抛出运行时异常就重试
+                .withWaitStrategy(WaitStrategies.fixedWait(2, TimeUnit.SECONDS))
+                .withStopStrategy(StopStrategies.stopAfterAttempt(3))   // 如果Stop的话就直接抛出RetryException异常了
+                .build();
+        Boolean call = retryer.call(() -> {
+            System.out.println("目标方法被调用了");
+            // 模拟返回false要重试
+            return false;
+        });
+    }
+
+    @Test
+    public void testResult() {
+        Retryer<Integer> retryer = RetryerBuilder.<Integer>newBuilder()
+                .retryIfResult(result -> result < 10)   // 返回值大于10的时候重试
+                // 实际使用时不要忘了设置StopStrategy，不然可能永远没法停止(默认值就是永不停止重试)
+                .withStopStrategy(StopStrategies.stopAfterAttempt(3))
+                .build();
         try {
-            Boolean call = retryer.call(() -> {
+            retryer.call(() -> {
                 System.out.println("目标方法被调用了");
-                // throw new RuntimeException("runtime exception...");
-                return false;
+                return 9;
             });
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (RetryException e) {
-            e.printStackTrace();
+        } catch (ExecutionException | RetryException e) {
+            System.out.println("尽了最大努力也没法获取目标接口调用的期望值，那就抛出异常");
         }
     }
 }
