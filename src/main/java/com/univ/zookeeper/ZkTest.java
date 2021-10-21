a5e0aff7610e649d7457454454684840d18b4528
univ <fcsnwu@163.com>
2021-10-21 17:37:25 +0800

    【zk】优化

--- src/main/java/com/univ/zookeeper/ZkTest.java
+++ src/main/java/com/univ/zookeeper/ZkTest.java
@@ -3,10 +3,13 @@ package com.univ.zookeeper;
 import java.io.IOException;
 
 import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 import org.junit.Test;
+
+import lombok.SneakyThrows;
 
 /**
  * @author univ
@@ -14,34 +17,30 @@ import org.junit.Test;
  */
 public class ZkTest {
 
-    /**
-     * 只是示例代码，不要在实际中使用
-     */
-    static ZooKeeper zooKeeper = null;
-
     @Test
     public void test() throws IOException, KeeperException, InterruptedException {
 
         String connectString = "127.0.0.1:2181";
         int sessionTimeOut = 5000;
         String zNode = "/zk_test";
-        zooKeeper = new ZooKeeper(connectString, sessionTimeOut, event -> {
-            String path = event.getPath();
-            Watcher.Event.KeeperState keeperState = event.getState();
-            Watcher.Event.EventType eventType = event.getType();
-            System.out.println("监听到节点变动了，znode:" + path);
-            System.out.println("监听到节点变动了，keeperState:" + keeperState.name());
-            System.out.println("监听到节点变动了，eventType:" + eventType.name());
-            try {
-                // 重新注册:一个watcher只能被触发一次(收到一次消息)
-                Stat stat = zooKeeper.exists(zNode, true);
-                System.out.println("stat:" + stat);
-            } catch (KeeperException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+
+        // watcher为null，表示不设置默认watcher
+        ZooKeeper zooKeeper = new ZooKeeper(connectString, sessionTimeOut, null);
+        Watcher watcher = new Watcher() {
+            @SneakyThrows
+            @Override
+            public void process(WatchedEvent event) {
+                System.out.println("process收到WatchedEvent事件了");
+                String path = event.getPath();
+                Stat stat = new Stat(); // 用来接收返回结果的stat状态，不是很面向对象(用入参接收返回值)
+                byte[] data = zooKeeper.getData(path, false, stat);
+                System.out.println("process znode的值为： " + new String(data));
+                System.out.println("返回znode的stat值为: " + stat);
             }
-        });
+        };
+        // 设置一个watcher，且丢弃返回znode的state信息
+        byte[] zNodeData = zooKeeper.getData(zNode, watcher, null);
+        System.out.println("znode的值为： " + new String(zNodeData));
 
         while (true) {
 
