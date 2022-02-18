e2aad2948ef4678401d81bc5b3a0e416c88135b9
univ <fcsnwu@163.com>
2022-02-18 11:35:21 +0800

    fix

--- src/main/java/com/univ/thirdutils/jackson/JacksonTest.java
+++ src/main/java/com/univ/thirdutils/jackson/JacksonTest.java
@@ -1,18 +1,14 @@
 package com.univ.thirdutils.jackson;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -30,38 +26,58 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class JacksonTest {
 
+    /**
+     * jackson最核心元素：pojo与java bean互转的桥梁
+     */
     private ObjectMapper objectMapper = new ObjectMapper();
 
+    /**
+     * json转java bean
+     * 核心方法：readValue
+     * 注：要转换的json来源可以是其它地方，如文件，URL，InputStream等。
+     * @throws IOException
+     */
     @Test
-    public void basic() throws IOException {
-        // 对象转json
+    public void pojoToBean() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        String str = objectMapper.writeValueAsString(new User());
-        System.out.println(str);    // {"name":"univ","age":28}
-
-
-        // json字符串转对象
         String jsonStr = "{\"name\":\"univ\",\"age\":28}";
-        // 第二个参数说明将jsonStr转换成User类
-        User user = objectMapper.readValue(jsonStr, User.class);
-        System.out.println(user);
+        JacksonBean jacksonBean = objectMapper.readValue(jsonStr, JacksonBean.class);
+        System.out.println(jacksonBean);
         // {"age":28,"birthday":"2018-12-08 08:39:30","nameV2":"univ"}
-        // 注意：结果中没有id字段，User类中name字段变成了nameV2，birthday是yyyy-MM-dd HH:mm:ss的格式
-
     }
 
     /**
-     * jsckson的常用注解
+     * java bean转json
+     * 核心方法：writeValueAsString
+     * 注：转成后的json可以存往其它地方，如文件、甚至OutputStream等，用writeValue方法
+     */
+    @Test
+    public void beanToPojo() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String str = objectMapper.writeValueAsString(new JacksonBean());
+        System.out.println(str);    // {"name":"univ","age":28}
+
+        // 写入到文件中
+        objectMapper.writeValue(new File("src/main/java/com/univ/thirdutils/jackson/file.txt"), new JacksonBean());
+    }
+
+    /**
+     * jackson的常用注解
      * @JsonIgnore
      * @JsonProperty
      * @JsonFormat
      */
     @Test
-    public void annotationJackson() throws JsonProcessingException {
-        User1 user1 = new User1();
+    public void annotationJackson() throws IOException {
+        JacksonBean2 jacksonBean2 = new JacksonBean2();
         ObjectMapper objectMapper = new ObjectMapper();
-        String jsonStr = objectMapper.writeValueAsString(user1);
+        String jsonStr = objectMapper.writeValueAsString(jacksonBean2);
         System.out.println(jsonStr);
+
+        // json串中必须有nameV2
+        String str = "{\"age\":28,\"birthday\":\"2022-02-17 19:22:52\",\"nameV2\":\"univ\", \"id\":100}";
+        JacksonBean2 user11 = objectMapper.readValue(str, JacksonBean2.class);
+        System.out.println(user11);
     }
 
     /**
@@ -69,19 +85,19 @@ public class JacksonTest {
      */
     @Test
     public void list() throws IOException {
-        User u1 = new User();
+        JacksonBean u1 = new JacksonBean();
         u1.setAge(20);
         u1.setName("aaa");
 
-        User u2 = new User();
+        JacksonBean u2 = new JacksonBean();
         u2.setAge(30);
         u2.setName("bbb");
 
-        User u3 = new User();
+        JacksonBean u3 = new JacksonBean();
         u3.setAge(40);
         u3.setName("ccc");
 
-        List<User> list = Arrays.asList(u1, u2, u3);
+        List<JacksonBean> list = Arrays.asList(u1, u2, u3);
         ObjectMapper objectMapper = new ObjectMapper();
         String str = objectMapper.writeValueAsString(list);
         System.out.println(str);
@@ -90,8 +106,8 @@ public class JacksonTest {
         String jsonStr = "[{\"name\":\"aaa\",\"age\":20},{\"name\":\"bbb\",\"age\":30},{\"name\":\"ccc\",\"age\":40}]";
 
         // json字符串转成数组
-        User[] users = objectMapper.readValue(jsonStr, User[].class);
-        System.out.println(Arrays.toString(users));
+        JacksonBean[] jacksonBeans = objectMapper.readValue(jsonStr, JacksonBean[].class);
+        System.out.println(Arrays.toString(jacksonBeans));
         // [User{name='aaa', age=20}, User{name='bbb', age=30}, User{name='ccc', age=40}]
 
         // json字符串转成list
@@ -112,14 +128,14 @@ public class JacksonTest {
         
         String jsonStr = "[{\"name\":\"aaa\",\"age\":20},{\"name\":\"bbb\",\"age\":30},{\"name\":\"ccc\",\"age\":40}]";
         ObjectMapper objectMapper = new ObjectMapper();
-        // 转成list
+        // 转成list ！！！错误用法！！！
         List list = objectMapper.readValue(jsonStr, List.class);
         System.out.println(list);
         // [{name=aaa, age=20}, {name=bbb, age=30}, {name=ccc, age=40}]
 
-        // 转成List<User>
-        JavaType listType = objectMapper.getTypeFactory().constructCollectionType(List.class, User.class);
-        List<User> list1 = objectMapper.readValue(jsonStr, listType);
+        // 转成List<User> ！！！正确用法！！！
+        JavaType listType = objectMapper.getTypeFactory().constructCollectionType(List.class, JacksonBean.class);
+        List<JacksonBean> list1 = objectMapper.readValue(jsonStr, listType);
         System.out.println(list1);
         // [User{name='aaa', age=20}, User{name='bbb', age=30}, User{name='ccc', age=40}]
         // 注意list1与list的差别
@@ -133,22 +149,22 @@ public class JacksonTest {
     public void jsonToMap() throws IOException {
         String mapStrJson = "{\"third\":{\"name\":\"ccc\",\"age\":40},\"first\":{\"name\":\"aaa\",\"age\":20},\"second\":{\"name\":\"bbb\",\"age\":30}}";
         // 转成map
-        JavaType mapType = objectMapper.getTypeFactory().constructMapType(HashMap.class, String.class, User.class);
-        Map<String, User> map = objectMapper.readValue(mapStrJson, mapType);
+        JavaType mapType = objectMapper.getTypeFactory().constructMapType(HashMap.class, String.class, JacksonBean.class);
+        Map<String, JacksonBean> map = objectMapper.readValue(mapStrJson, mapType);
         System.out.println(map);
         // {third=User{name='ccc', age=40}, first=User{name='aaa', age=20}, second=User{name='bbb', age=30}}
     }
 
     /**
-     * json字符串转成集合泛型
+     * json字符串转成泛型
      * @throws IOException
      */
     @Test
-    public void jsonToMighty() throws IOException {
+    public void generic() throws IOException {
         /*Author author = new Author();
         author.setAge(20);
         author.setName("abc");
-
+JsonTypeInfo
         Book<Author> book = new Book<>();
         book.setValue(author);
         book.setName("book");
@@ -158,145 +174,9 @@ public class JacksonTest {
         // {"name":"book","value":{"name":"abc","age":20}}
 
         String jsonStr = "{\"name\":\"book\",\"value\":{\"name\":\"abc\",\"age\":20}}";
-        JavaType javaType = objectMapper.getTypeFactory().constructParametricType(Book.class, Author.class);
-        Book<Author> book1 = objectMapper.readValue(jsonStr, javaType);
-        System.out.println(book1);
+        JavaType javaType = objectMapper.getTypeFactory().constructParametricType(GenericBook.class, Author.class);
+        GenericBook<Author> genericBook1 = objectMapper.readValue(jsonStr, javaType);
+        System.out.println(genericBook1);
         // Book{name='book', value=Author{name='abc', age=20}}
-
-    }
-}
-
-class User {
-    private String name = "univ";
-    private Integer age = 28;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-}
-
-class User1 {
-
-    @JsonIgnore // 序列化时忽略此字段
-    private Long id = 10l;
-
-    @JsonProperty("nameV2") // 将name属性映射成nameV2
-    private String name = "univ";
-
-    private Integer age = 28;
-
-    // 注意不要忘了timezone = "GMT+8"
-    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
-    private Date birthday = new Date();
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
-}
-
-class Book<T> {
-    private String name;
-    private T value;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public T getValue() {
-        return value;
-    }
-
-    public void setValue(T value) {
-        this.value = value;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "name='" + name + '\'' +
-                ", value=" + value +
-                '}';
-    }
-}
-
-class Author {
-    private String name;
-    private Integer age;
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Author{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
     }
 }
