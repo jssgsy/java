f45b4e4c3587604fad8945b48333797de7d5286b
univ <fcsnwu@163.com>
2022-04-06 15:12:00 +0800

    【fastjson】

---
+++ src/main/java/com/univ/thirdutils/fastjson/FastJsonTest.java
@@ -0,0 +1,16 @@
+package com.univ.thirdutils.fastjson;
+
+import org.junit.Test;
+
+/**
+ * @author univ
+ * 2022/2/17 5:04 下午
+ */
+public class FastJsonTest {
+
+    @Test
+    public void test() {
+
+        // JSONObject.parseObject();
+    }
+}
---
+++ src/main/java/com/univ/thirdutils/fastjson/README.md
@@ -0,0 +1,5 @@
+# 简介
+阿里巴巴出品。
+[github](https://github.com/alibaba/fastjson)
+与jackson一样，是一个java bean与json字符串互转的工具。
+
