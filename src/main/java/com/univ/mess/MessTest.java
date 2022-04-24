d785bc287b974e17999849b08306f370571d38b6
univ <fcsnwu@163.com>
2022-04-24 11:10:44 +0800

    fix

--- src/main/java/com/univ/mess/MessTest.java
+++ src/main/java/com/univ/mess/MessTest.java
@@ -1,7 +1,5 @@
 package com.univ.mess;
 
-import javax.naming.InvalidNameException;
-
 import org.junit.Test;
 
 /**
@@ -10,19 +8,8 @@ import org.junit.Test;
  */
 public class MessTest<Item> {
 
-    /**
-     * 栈的常用方法:
-     * push();//入栈,Pushes an item onto the top of this stack.
-     * pop()://出栈,并返回出栈元素
-     * peek()://获取栈顶元素,但不出栈
-     */
     @Test
-    public void test1() throws InterruptedException, InvalidNameException {
-
-
-
-
-
+    public void test1() {
 
     }
 
