f07361ce5633a53efa0571dea4f54ede7f5ebad6
univ <fcsnwu@163.com>
2023-05-17 09:45:50 +0800

    comment

--- src/main/java/com/univ/concurrent/CountDownLatchTest.java
+++ src/main/java/com/univ/concurrent/CountDownLatchTest.java
@@ -7,7 +7,7 @@ import java.util.concurrent.CountDownLatch;
  * @datetime 2018/11/7 7:16 PM
  * @description
  * 1. CountDownLatch、CyclicBarrier、Semaphore均是jdk1.5中引入的，是一个并发编程辅助类；
- * 2. CountDown的使用场景：任务A需要在任务B、C、D任务执行完之后再执行，(A，B，C，D均由不同的线程完成)，此时可用CountDownLatch完成。其实就是，线程A需要在线程B、C、D执行完后才能执行；
+ * 2. CountDownLatch的使用场景：任务A需要在任务B、C、D任务执行完之后再执行，(A，B，C，D均由不同的线程完成)，此时可用CountDownLatch完成。其实就是，线程A需要在线程B、C、D执行完后才能执行；
  * 3. CountDownLatch的用法很简单，下例模拟了主线程必须在其它线程执行完后才能继续执行；
  * 4. CountDownLatch不可重用；(参见CyclicBarrierTest)
  */
@@ -16,7 +16,7 @@ import java.util.concurrent.CountDownLatch;
 /**
  *
  * 1. 构造器中的计数值（count）实际上就是闭锁需要等待的线程数量。这个值只能被设置一次，而且CountDownLatch没有提供任何机制去重新设置这个计数值。
- * 2. 与CountDownLatch的第一次交互是主线程等待其他线程。主线程必须在启动其他线程后立即调用CountDownLatch.await()方法。这样主线程的操作就会在这个方法上阻塞，直到其他线程完成各自的任务。
+ * 2. 注意：CountDownLatch的使用场景是一个线程需要等待其它线程执行后(其实就是调用countDown方法)再执行，线程间不一定具有父子关系；
  * 3. CountDownLatch的核心方法：
  *     a. await();调用await()方法的线程会被挂起，它会等待直到count值为0才继续执行
  *     b. await(long timeout, TimeUnit unit);和await()类似，只不过等待一定的时间后count值还没变为0的话就会继续执行
@@ -25,7 +25,6 @@ import java.util.concurrent.CountDownLatch;
 public class CountDownLatchTest {
 
     public static void main(String[] args) {
-        // 构造函数就是需要等待的线程数量
         CountDownLatch countDownLatch = new CountDownLatch(2);
         for (int i = 0; i < 2; i++) {   // 启动两个线程
             new Thread(() -> {
@@ -44,7 +43,7 @@ public class CountDownLatchTest {
         try {
             // 调用wait方法会让执行此方法的线程(即这里的主线程)暂停直到其它2个线程(构造函数中设置的)执行完成再执行
             countDownLatch.await();
-            System.out.println(Thread.currentThread().getName() + " 执行结束了(一定在子线程执行结束后运行)");
+            System.out.println("主线程：" + Thread.currentThread().getName() + " 执行结束了");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
