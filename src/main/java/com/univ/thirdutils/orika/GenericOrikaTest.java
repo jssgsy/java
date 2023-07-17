451c36caacc1bef62a700d12bb8f12fa66ff5322
univ <fcsnwu@163.com>
2023-07-17 16:27:07 +0800

    comment fix

--- src/main/java/com/univ/thirdutils/orika/GenericOrikaTest.java
+++ src/main/java/com/univ/thirdutils/orika/GenericOrikaTest.java
@@ -36,13 +36,24 @@ public class GenericOrikaTest {
         ArrayList<G1DTO> g1DTOS = new ArrayList<>();
         g1DTOS.add(g1DTO);
 
-        // 指定源类型为：List<G1DTO>
+        // 指定源类型为：G1DTO, 注意，不是List<G1DTO>，因为这里调用的就是mapAsList方法
         Type<G1DTO> sourceType = new TypeBuilder<G1DTO>(){}.build();
-        // 指定目标类型为：List<G1>
+
+        // 指定目标类型为：G1, 注意，不是List<G1>，因为这里调用的就是mapAsList方法
         Type<G1> destType = new TypeBuilder<G1>(){}.build();
 
         List<G1> map = mapperFacade.mapAsList(g1DTOS, sourceType, destType);
         System.out.println(map);
+
+        // 经验证，这里没法使用map方法来达到相同的目标。
+        // 看来要明确指定是转换成对象还是转换成集合。
+        // 如下代码是不ok的
+        // 指定目标类型为：List<G1DTO>
+        /*Type<List<G1DTO>> sourceType2 = new TypeBuilder<List<G1DTO>>(){}.build();
+        Type<List<G1>> destType2 = new TypeBuilder<List<G1>>(){}.build();
+        List<G1> map2 = mapperFacade.map(g1DTOS, sourceType2, destType2);
+        System.out.println(map2);   // 输出：[]*/
+
     }
 
 }
