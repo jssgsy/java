8be4762300633dc945d796e6098fe9d1eacbf816
univ <fcsnwu@163.com>
2023-07-17 15:05:20 +0800

    fastjson 泛型转换 TypeReference

--- src/main/java/com/univ/thirdutils/fastjson/FastJsonTest.java
+++ src/main/java/com/univ/thirdutils/fastjson/FastJsonTest.java
@@ -1,6 +1,11 @@
 package com.univ.thirdutils.fastjson;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import lombok.Data;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * @author univ
@@ -13,4 +18,33 @@ public class FastJsonTest {
 
         // JSONObject.parseObject();
     }
+
+    /**
+     * 转换成泛型
+     * 借助TypeReference
+     * 底层是 ParameterizedType
+     */
+    @Test
+    public void genericParse() {
+        String json = "[{\"name\":\"zhangsan\", \"address\":\"hangzhou\"}]";
+        // 指定要转换成的类型为：List<Ge1>
+        List<Ge1> g1s = JSONObject.parseObject(json, new TypeReference<List<Ge1>>() {});
+        System.out.println("ge1：" + g1s);
+
+        // 指定要转换成的类型为：List<Ge2>
+        List<Ge2> g2s = JSONObject.parseObject(json, new TypeReference<List<Ge2>>() {});
+        System.out.println("g2s：" + g2s);
+    }
+}
+
+@Data
+class Ge1 {
+    private String name;
+    private String address;
+}
+
+@Data
+class Ge2 {
+    private String name;
+    private String address;
 }
