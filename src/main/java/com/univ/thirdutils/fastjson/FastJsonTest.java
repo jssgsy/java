f969fe3f52c929df73ad04059d2fcb22552025e8
univ <fcsnwu@163.com>
2023-08-25 14:12:06 +0800

    fastjson处理时间相关类型

--- src/main/java/com/univ/thirdutils/fastjson/FastJsonTest.java
+++ src/main/java/com/univ/thirdutils/fastjson/FastJsonTest.java
@@ -2,9 +2,13 @@ package com.univ.thirdutils.fastjson;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import lombok.Data;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -48,6 +52,28 @@ public class FastJsonTest {
         MyObj<Ge1> parsed = JSONObject.parseObject(json, new TypeReference<MyObj<Ge1>>() {});
         System.out.println(parsed);
     }
+
+    // 对各种时间类型的默认处理
+    @Test
+    public void timeDefault() {
+        System.out.println(JSONObject.toJSONString(new TimeObj()));
+        // {"date":1692933166417,"localDate":"2023-08-25","localDateTime":"2023-08-25T11:12:46.416"}
+    }
+
+    /**
+     * date类型的默认字符串形式
+     */
+    @Test
+    public void timeDateStrDefault() {
+        System.out.println(JSONObject.toJSONString(new TimeObj(), SerializerFeature.WriteDateUseDateFormat));
+        // {"date":"2023-08-25 11:51:45","localDate":"2023-08-25","localDateTime":"2023-08-25T11:51:45.687"}
+    }
+
+    @Test
+    public void timeDateCustomize() {
+        System.out.println(JSONObject.toJSONStringWithDateFormat(new TimeObj(), "yyyy/MM/dd HH:mm:ss", SerializerFeature.WriteDateUseDateFormat));
+        // {"date":"2023/08/25 13:43:12","localDate":"2023-08-25","localDateTime":"2023/08/25 13:43:12"}
+    }
 }
 
 @Data
@@ -65,4 +91,19 @@ class Ge2 {
 @Data
 class MyObj<T> {
     T data;
+}
+
+/**
+ * JSONField注解适用于所有时间类型，如LocalDateTime、LocalDate、Date等
+ */
+@Data
+class TimeObj {
+//    @JSONField(format = "yyyy-MM-dd HH:mm:ss", name = "llll")
+    private LocalDateTime localDateTime = LocalDateTime.now();
+
+//    @JSONField(format = "yyyy/MM/dd")
+    private LocalDate localDate = LocalDate.now();
+
+//    @JSONField(format = "yyyy/MM/dd HH:mm:ss")
+    private Date date = new Date();
 }
