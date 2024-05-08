7837ecf3e62c275b1d70ccf1087ce36451301de5
univ <fcsnwu@163.com>
2024-05-08 17:02:55 +0800

    doc

--- src/main/java/com/univ/thirdutils/easyexcel/EasyPO.java
+++ src/main/java/com/univ/thirdutils/easyexcel/EasyPO.java
@@ -2,6 +2,7 @@ package com.univ.thirdutils.easyexcel;
 
 import com.alibaba.excel.annotation.ExcelProperty;
 import com.alibaba.excel.annotation.format.DateTimeFormat;
+import com.alibaba.excel.converters.date.DateStringConverter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,9 +24,11 @@ public class EasyPO {
     @ExcelProperty(value = "年龄", order = 1)
     private Integer age;
 
-    // DateStringConverter默认是使用yyyy-MM-dd HH:mm:ss
-    @ExcelProperty(value = "生日", order = 2)
+    // DateStringConverter默认主是使用yyyy-MM-dd HH:mm:ss，所以可省略
+    // 不指定converter也不使用@DateTimeFormat，easyexcel默认会按照yyyy-MM-dd HH:mm:ss格式写
+    @ExcelProperty(value = "生日", order = 2, converter = DateStringConverter.class)
     // 这里用@DateTimeFormat来指定pattern为yyyy-MM-dd
+    // 使用了@DateTimeFormat，就不必再指定converter了，即使指定也会优先使用@DateTimeFormat中的pattern
     // 也可以作用在LocalDate、LocalDateTime等类型上
     @DateTimeFormat(value = "yyyy-MM-dd")
     private Date birthday;
