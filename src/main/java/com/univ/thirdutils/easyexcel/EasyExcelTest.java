58f106295e6e52e71564e4d9a58ab0b4732e0ba1
univ <fcsnwu@163.com>
2024-05-09 11:31:47 +0800

    feat(easyexcel写入多个sheet):

--- src/main/java/com/univ/thirdutils/easyexcel/EasyExcelTest.java
+++ src/main/java/com/univ/thirdutils/easyexcel/EasyExcelTest.java
@@ -1,11 +1,13 @@
 package com.univ.thirdutils.easyexcel;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.read.builder.ExcelReaderBuilder;
 import com.alibaba.excel.read.listener.ReadListener;
 import com.alibaba.excel.write.builder.ExcelWriterBuilder;
 import com.alibaba.excel.write.builder.ExcelWriterSheetBuilder;
+import com.alibaba.excel.write.metadata.WriteSheet;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -65,12 +67,7 @@ public class EasyExcelTest {
     }
 
     @Test
-    public void write() {
-        List<EasyPO> dataList = Arrays.asList(
-                new EasyPO("aaax", 10, new Date()),
-                new EasyPO("bbby", 20, new Date()),
-                new EasyPO("cccz", 30, new Date())
-        );
+    public void writeBasic() {
         String excelFilePath = "data/excel/2007.xlsx";
         // 1.1. 获取ExcelWriterBuilder对象
         ExcelWriterBuilder writerBuilder = EasyExcel.write(excelFilePath, EasyPO.class);
@@ -84,7 +81,38 @@ public class EasyExcelTest {
 
         // 3. 实际写入
         // 是由ExcelWriterSheetBuilder对象写而不是writerBuilder
-        sheetBuilder.doWrite(dataList);
+        sheetBuilder.doWrite(data());
+    }
+
+    /**
+     * 同时写多个sheet
+     * 这里要注意WriteSheet实例的获取
+     */
+    @Test
+    public void writeMultiSheet() {
+        String excelFilePath = "data/excel/2007.xlsx";
+        ExcelWriterBuilder excelWriterBuilder = EasyExcel.write(excelFilePath, EasyPO.class);
+
+        // 注意WriteSheet的实例方法，必须使用EasyExcel.writerSheet，此时创建的Sheet和ExcelWriter没关联
+        // 所以可被任意ExcelWriter对象写入数据
+        WriteSheet writeSheet1 = EasyExcel.writerSheet(0, "第一个sheet").build();
+        WriteSheet writeSheet2 = EasyExcel.writerSheet(1, "第二个sheet").build();
+        // 下面获取实例方法是不ok，因为此时已经和excelWriterBuilder关联了
+        // WriteSheet writeSheet = excelWriterBuilder.sheet().build();
+        // 每调用一次build方法会新建一个ExcelWriter对象
+        try (ExcelWriter excelWriter = excelWriterBuilder.build()) {
+            excelWriter.write(data(), writeSheet1);
+            excelWriter.write(data(), writeSheet2);
+        }
+    }
+
+
+    private List<EasyPO> data() {
+        return Arrays.asList(
+                new EasyPO("aaax", 10, new Date()),
+                new EasyPO("bbby", 20, new Date()),
+                new EasyPO("cccz", 30, new Date())
+        );
     }
 
 
