3c5b398ab2175d080af29ea3ee58247cd271560a
univ <fcsnwu@163.com>
2025-12-16 16:14:21 +0800

    poi 不限层级的复杂合并表头 写入数据

--- src/main/java/com/univ/thirdutils/poi/PoiComplicateHeadTest.java
+++ src/main/java/com/univ/thirdutils/poi/PoiComplicateHeadTest.java
@@ -11,8 +11,12 @@ import org.junit.Test;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+
 
 /**
  * 支持不限层级的复杂合并表头
@@ -44,9 +48,101 @@ public class PoiComplicateHeadTest {
             for (FieldConfig fieldConfig : headList) {
                 currentColIndex = appendHead(sheet, 0, fieldConfig, treeDepth, currentColIndex, headerStyle);
             }
+
+            // 同时写入表格数据，注释此行则只生成表头
+            writeTableData(sheet, treeDepth, dataModelList(), headList);
+
             try (FileOutputStream fos = new FileOutputStream("data/excel/多级复杂表头.xlsx")) {
                 workbook.write(fos);
             }
+        }
+    }
+
+
+    /**
+     * 从dataModel实例中获取指定属性的值
+     * 注：这里得处理数据类型问题，写到单元格中的值(cell.setCellValue方法)必须指定数据类型，不能用Object
+     *
+     * @param dataModel 指定实例
+     * @param name 指定属性
+     * @return 指定实例对应的指定属性的值
+     */
+    private Object getPropertyValueFromObj(DataModel dataModel, String name) {
+        try {
+            Field field = DataModel.class.getDeclaredField(name);
+            field.setAccessible(true);
+            return field.get(dataModel);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            return "unknown";
+        }
+    }
+
+    /**
+     * 按照顺序给所有叶子节点编号(列号)，处理整个森林
+     * @param headList 整个表头(即整个森林)，即表头元数据
+     * @return key：colIndex， value：对应的表头元数据(FieldConfig)
+     *  重点：之所以value是FieldConfig，是因为要从尝试从数据中获取对应列名的值，FieldConfig是媒介
+     */
+    private Map<Integer, FieldConfig> toColIndexMap(List<FieldConfig> headList) {
+        Map<Integer, FieldConfig> colIndexMap = new LinkedHashMap<>();
+        int currentColIndex = 0;
+        for (FieldConfig fieldConfig : headList) {
+            currentColIndex = calculateColIndexMap(fieldConfig, currentColIndex, colIndexMap);
+        }
+        return colIndexMap;
+    }
+
+    /**
+     * 按照顺序给所有叶子节点编号(列号)，处理一棵树
+     *
+     * @param fieldConfig 森林中的一棵树，即表头元数据
+     * @param currentColIndex 当前列索引
+     * @param colIndexMap 用来收集结果 key：colIndex， value：对应的表头元数据(FieldConfig)
+     * @return 下一个要处理的叶子节点所在的列
+     */
+    private int calculateColIndexMap(FieldConfig fieldConfig, int currentColIndex, Map<Integer, FieldConfig> colIndexMap) {
+        if (CollectionUtils.isEmpty(fieldConfig.children)) {
+            colIndexMap.put(currentColIndex, fieldConfig);
+            currentColIndex++;
+            return currentColIndex;
+        } else {
+            for (FieldConfig config : fieldConfig.children) {
+                currentColIndex = calculateColIndexMap(config, currentColIndex, colIndexMap);
+            }
+            return currentColIndex;
+        }
+    }
+
+    /**
+     * 将数据写入多级表格
+     * 核心思路：表格数据适配表头结构，具体的：
+     *  遍历表头，构建(叶子节点所在的列, 叶子节点)组成的map；
+     *  遍历表格数据，遍历表头(此时是叶子节点)，找出其在对应dataModel中的值并写入
+     *
+     * @param sheet 当前sheet页，即创建了多级表头的sheet页
+     * @param rowIndexStart 从这一行开始写入
+     * @param dataModelList 表格数据
+     * @param headList 表头数据
+     */
+    private void writeTableData(XSSFSheet sheet,
+                                int rowIndexStart,
+                                List<DataModel> dataModelList,
+                                List<FieldConfig> headList) {
+        Map<Integer, FieldConfig> colIndexMap = toColIndexMap(headList);
+
+        // 逐行数据处理
+        for (DataModel dataModel : dataModelList) {
+            // 创建行
+            XSSFRow row = sheet.createRow(rowIndexStart);
+            // 按顺序尝试给对应单元格填充值
+            colIndexMap.forEach((colIndex, fieldConfig) -> {
+                // 创建列
+                XSSFCell cell = row.createCell(colIndex);
+                String cellValue = getPropertyValueFromObj(dataModel, fieldConfig.getName()).toString();
+                cell.setCellValue(cellValue);
+            });
+            // 处理下一个数据
+            rowIndexStart++;
         }
     }
 
@@ -170,6 +266,13 @@ public class PoiComplicateHeadTest {
         return Arrays.asList(f1, f2, f3);
     }
 
+    private List<DataModel> dataModelList() {
+        return Arrays.asList(
+                new DataModel("a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "x", "xx", "xxx"),
+                new DataModel("b", "bb", "bbb", "bbbb", "bbbbb", "bbbbbb", "y", "yy", "yyy"),
+                new DataModel("c", "cc", "ccc", "cccc", "ccccc", "cccccc", "z", "zz", "zzz")
+        );
+    }
     private List<FieldConfig> threeLevelHead() {
         // 构造表头数据
         FieldConfig f1 = new FieldConfig("f1", null);
@@ -191,17 +294,17 @@ public class PoiComplicateHeadTest {
         // 构造表头数据
         FieldConfig f1 = new FieldConfig("f1", null);
         FieldConfig f2 = new FieldConfig("f2", Arrays.asList(
-                new FieldConfig("f2-1", null),
-                new FieldConfig("f2-2", Arrays.asList(
-                        new FieldConfig("f2-2-1", null),
-                        new FieldConfig("f2-2-2", null))),
-                new FieldConfig("f2-3", Arrays.asList(
-                        new FieldConfig("f2-3-1", null),
-                        new FieldConfig("f2-3-2", Arrays.asList(
-                                new FieldConfig("f2-3-2-1", null),
-                                new FieldConfig("f2-3-2-2", null)))
+                new FieldConfig("f2_1", null),
+                new FieldConfig("f2_2", Arrays.asList(
+                        new FieldConfig("f2_2_1", null),
+                        new FieldConfig("f2_2_2", null))),
+                new FieldConfig("f2_3", Arrays.asList(
+                        new FieldConfig("f2_3_1", null),
+                        new FieldConfig("f2_3_2", Arrays.asList(
+                                new FieldConfig("f2_3_2_1", null),
+                                new FieldConfig("f2_3_2_2", null)))
                 )),
-                new FieldConfig("f2-3-3", null)));
+                new FieldConfig("f2_3_3", null)));
         FieldConfig f3 = new FieldConfig("f3", null);
         return Arrays.asList(f1, f2, f3);
     }
@@ -233,5 +336,20 @@ public class PoiComplicateHeadTest {
     static class FieldConfig {
         private String name;
         private List<FieldConfig> children;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class DataModel {
+        // 简单起见，这里的名字就和表格的列名保持一致，也可以在FieldConfig中指定dataKey；
+        private String f1;
+        private String f2_1;
+        private String f2_2_1;
+        private String f2_2_2;
+        private String f2_3_1;
+        private String f2_3_2_1;
+        private String f2_3_2_2;
+        private String f2_3_3;
+        private String f3;
     }
 }
